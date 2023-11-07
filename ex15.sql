488f23bf88d2b2f8f27ce2d0d703911572f733c4
aume-gp <77577837+aume-gp@users.noreply.github.com>
2023-11-07 17:41:31 +0100

    Réindentation du fichier

--- ex15.sql
+++ ex15.sql
@@ -1,13 +1,13 @@
 --Requête 15. Trouver les entités et officiers associés qui ont une relation avant une certaine date (par exemple, le 1er janvier 2010) :
 SELECT
-    entity.name AS name_entity,
-    officer.name AS name_officer,
-    assoc_officer_entity.start_date
+  entity.name AS name_entity,
+  officer.name AS name_officer,
+  assoc_officer_entity.start_date
 FROM
-    assoc_officer_entity
+  assoc_officer_entity
 JOIN
-    entity ON assoc_officer_entity.entity = entity.id
+  entity ON assoc_officer_entity.entity = entity.id
 JOIN
-    officer ON assoc_officer_entity.officer = officer.id
+  officer ON assoc_officer_entity.officer = officer.id
 WHERE
-    assoc_officer_entity.start_date < '2010-01-01';
+  assoc_officer_entity.start_date < '2010-01-01';
