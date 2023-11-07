e0a7b38baa63c33a452f91e28541900dea9a2df0
aume-gp <77577837+aume-gp@users.noreply.github.com>
2023-11-07 17:41:44 +0100

    Réindentation du fichier

--- ex16.sql
+++ ex16.sql
@@ -1,10 +1,10 @@
 --Requête 16. Trouver le nombre d'entités par pays :
 SELECT
-    country.country,
-    COUNT(entity.id) AS number_entity
+  country.country,
+  COUNT(entity.id) AS number_entity
 FROM
-    entity
-    JOIN address ON entity.id_address = address.id_address
-    JOIN country ON address.countries = country.code
+  entity
+  JOIN address ON entity.id_address = address.id_address
+  JOIN country ON address.countries = country.code
 GROUP BY
-    country.country;
+  country.country;
