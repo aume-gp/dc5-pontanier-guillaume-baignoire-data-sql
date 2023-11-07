b9eb297fb2c6f72d59c07603e4741847527b03d6
aume-gp <77577837+aume-gp@users.noreply.github.com>
2023-11-07 17:42:38 +0100

    Réindentation du fichier

--- ex18.sql
+++ ex18.sql
@@ -1,11 +1,11 @@
 --Requête 18. Trouver les entités qui ont des associations avec des officiers de plus de 2 pays différents :
 SELECT
-    entity.name
+  entity.name
 FROM
-    entity
-    JOIN assoc_officer_entity ON entity.id = assoc_officer_entity.entity
-    JOIN officer ON assoc_officer_entity.officer = officer.id
+  entity
+  JOIN assoc_officer_entity ON entity.id = assoc_officer_entity.entity
+  JOIN officer ON assoc_officer_entity.officer = officer.id
 GROUP BY
-    entity.name
+  entity.name
 HAVING
-    COUNT(DISTINCT officer.country_id) > 2
+  COUNT(DISTINCT officer.country_id) > 2;
