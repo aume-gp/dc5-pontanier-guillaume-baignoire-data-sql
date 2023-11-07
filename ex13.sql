f05154ccdb4d20e82da78577423c3caca58aa68f
aume-gp <77577837+aume-gp@users.noreply.github.com>
2023-11-07 17:40:59 +0100

    Réindentation du fichier

--- ex13.sql
+++ ex13.sql
@@ -1,17 +1,17 @@
 --Requête 13. Lister les intermédiaires et le nombre d'entités et d'officiers associés :
 SELECT
-    intermediary.name,
-    COUNT(DISTINCT entity.id) AS number_entity,
-    COUNT(DISTINCT officer.id) AS number_officer
+  intermediary.name,
+  COUNT(DISTINCT entity.id) AS number_entity,
+  COUNT(DISTINCT officer.id) AS number_officer
 FROM
-    intermediary
+  intermediary
 JOIN
-    assoc_inter_entity ON assoc_inter_entity.inter = intermediary.id
+  assoc_inter_entity ON assoc_inter_entity.inter = intermediary.id
 JOIN
-    entity ON assoc_inter_entity.entity = entity.id
+  entity ON assoc_inter_entity.entity = entity.id
 JOIN
-    assoc_officer_entity ON assoc_officer_entity.entity = entity.id
+  assoc_officer_entity ON assoc_officer_entity.entity = entity.id
 JOIN
-    officer ON assoc_officer_entity.officer = officer.id
+  officer ON assoc_officer_entity.officer = officer.id
 GROUP BY
-    intermediary.name;
+  intermediary.name;
