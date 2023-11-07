f0c768b435e9d7dbd8c93fe8ee335e655f485354
aume-gp <77577837+aume-gp@users.noreply.github.com>
2023-11-07 16:20:45 +0100

    Push ex17

---
+++ ex17.sql
@@ -0,0 +1,11 @@
+--Requête 17. Lister les intermédiaires qui ont des associations avec plus de 5 entités :
+SELECT
+    intermediary.name,
+    COUNT(assoc_inter_entity.entity) AS number_entity
+FROM
+    intermediary
+    JOIN assoc_inter_entity ON intermediary.id = assoc_inter_entity.inter
+GROUP BY
+    intermediary.name
+HAVING
+    COUNT(assoc_inter_entity.entity) > 5
